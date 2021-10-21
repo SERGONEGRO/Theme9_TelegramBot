b8e7753701563ad37806c59c03ab4dc7a13fecb4
Сергей Воробьев <kazan1003@gmail.com>
2021-10-21 17:05:19 +0300

    document download complete

--- Theme9_TelegramBot/Program.cs
+++ Theme9_TelegramBot/Program.cs
@@ -1,15 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+
+// Создать бота, позволяющего принимать разные типы файлов, 
+// *Научить бота отправлять выбранный файл в ответ
+// 
+// https://data.mos.ru/
+// https://apidata.mos.ru/
+// 
+// https://vk.com/dev
+// https://vk.com/dev/manuals
+
+// https://dev.twitch.tv/
+// https://discordapp.com/developers/docs/intro
+// https://discordapp.com/developers/applications/
+// https://discordapp.com/verification
+
 
 namespace Theme9_TelegramBot
 {
     class Program
     {
+        static TelegramBotClient bot;
+        static string path = @"d:\\bot\";
+
         static void Main(string[] args)
         {
+            string token = File.ReadAllText(@"C:\Users\User\YandexDisk\token1.txt");
+
+            bot = new TelegramBotClient(token);
+            bot.OnMessage += MessageListener;
+            bot.StartReceiving();
+            Console.ReadKey();
         }
+        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+
+            //документы
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+                string pathDocument = path + e.Message.Document.FileName;
+                DownLoad(e.Message.Document.FileId, pathDocument);
+                Console.WriteLine($"Файл загружен в {pathDocument}");
+            }
+
+            //Аудио
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Audio)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+
+                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+            }
+
+            //видео
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Video)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+
+                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+            }
+
+            if (e.Message.Text == null) return;
+
+            var messageText = e.Message.Text;
+
+
+            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                $"Сам {messageText}"
+                );
+        }
+
+        static async void DownLoad(string fileId, string path)
+        {
+            var file = await bot.GetFileAsync(fileId);
+            FileStream fs = new FileStream(path, FileMode.Create);
+            await bot.DownloadFileAsync(file.FilePath, fs);
+            fs.Close();
+
+            fs.Dispose();
+        }
+
     }
 }
